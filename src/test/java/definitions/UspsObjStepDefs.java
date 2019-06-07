ec6911fc2ae93e6fb98d5f01c99724b19ceaec56
Felix <tierleybob@gmail.com>
2019-06-06 21:26:09 -0700

    test commit, remove unused import

--- src/test/java/definitions/UspsObjStepDefs.java
+++ src/test/java/definitions/UspsObjStepDefs.java
@@ -1,10 +1,8 @@
 package definitions;
 
-import cucumber.api.PendingException;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
-import pages.SampleForm;
 import pages.UspsHome;
 import pages.UspsLabels;
 import pages.UspsPostalStore;
@@ -14,7 +12,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class UspsObjStepDefs {
 
     @When("^I go to \"([^\"]*)\"$")
-    public void iGoTo(String link) throws Throwable {
+    public void iGoTo(String link) {
         switch (link) {
             case "labels":
                 new UspsHome().clickLabels();
@@ -31,31 +29,31 @@ public class UspsObjStepDefs {
     }
 
     @Then("^I verify \"([^\"]*)\" section exists$")
-    public void iVerifySectionExists(String filterText) throws Throwable {
+    public void iVerifySectionExists(String filterText) {
         String priceFilters = new UspsPostalStore().getPriceFilters();
         assertThat(priceFilters).contains(filterText);
     }
 
     @When("^I sort by \"([^\"]*)\"$")
-    public void iSortBy(String how) throws Throwable {
+    public void iSortBy(String how) {
         new UspsPostalStore().sortBy(how);
     }
 
     @Then("^I verify that \"([^\"]*)\" is cheapest$")
-    public void iVerifyThatIsCheapest(String item) throws Throwable {
+    public void iVerifyThatIsCheapest(String item) {
         String firstFound = new UspsPostalStore().getFirstFoundItem();
         assertThat(firstFound).contains(item);
     }
 
     @Then("^I verify that \"([^\"]*)\" is required$")
-    public void iVerifyThatIsRequired(String arg0) throws Throwable {
+    public void iVerifyThatIsRequired(String arg0) {
         UspsLabels labelsPage = new UspsLabels();
         labelsPage.clickSignIn();
         assertThat(labelsPage.usernameErrorVisible()).isTrue();
     }
 
     @Then("^I verify that \"([^\"]*)\" is possible$")
-    public void iVerifyThatIsPossible(String expectedText) throws Throwable {
+    public void iVerifyThatIsPossible(String expectedText) {
         UspsLabels labelsPage = new UspsLabels();
         String actualText = labelsPage.getSignUpText();
         assertThat(actualText).containsIgnoringCase(expectedText);
