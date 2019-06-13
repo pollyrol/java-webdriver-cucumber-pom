26a3648ce5d9903144464d34ebfea32e680e20fd
neolost <neobut9393@gmail.com>
2019-06-12 19:40:20 -0700

    changes

--- src/test/java/pages/CareersLogin.java
+++ src/test/java/pages/CareersLogin.java
@@ -3,7 +3,6 @@ package pages;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.io.FileNotFoundException;
 
 
 public class CareersLogin extends Page {
